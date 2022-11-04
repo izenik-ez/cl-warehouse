23515f266ef69db1458a7942f732a1ad5413aa6d
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-11-04 18:58:00 +0100

    Add server capabilities to tests

--- tests/cl-warehouse.lisp
+++ tests/cl-warehouse.lisp
@@ -2,7 +2,10 @@
 (defpackage :cl-warehouse-test
   (:use :cl
         :cl-warehouse
-        :fiveam)
+        :fiveam
+   :dexador)
+  (:import-from :cl-warehouse :start :stop)
+  (:shadowing-import-from :dexador :get :delete)
   (:export :all-tests :run-warehouse-tests))
 
 (in-package :cl-warehouse-test)
@@ -14,8 +17,44 @@
   :description "Root for warehouse tests")
 (in-suite all-tests)
 
-(test init (is-true (= 0 0)))
+
+(defvar *handler* nil)
+
+(test init (is (= 0 0) "OK" "Not ok"))
+
+
+
+(defvar *host* "localhost")
+(defvar *port* 5001)
+(defvar *url* (concatenate 'string
+                           "http://"
+                           *host*
+                           ":"
+                           (write-to-string *port*)))
+
+(defun add-page(page)
+  (concatenate 'string *url* page))
+
+(test index (multiple-value-bind (body status)
+               (dex:get *url*)
+             (declare (ignore body))
+              (is (= status 200) "OK" "Wrong!")))
+
+(test signal-an-error
+  (signals http-request-not-found (dex:get (add-page "non-existing.html"))))
+    
 
 (defun run-warehouse-tests ()
-  (fiveam:run! 'all-tests))
-(
+  (let ((*handler* nil)
+        (dex:*use-connection-pool* nil))
+;;        (dexador.connection-cache::*threads-connection-pool*
+;;          (make-hash-table :test 'equal :synchronized t)))
+    (when *handler*
+      (cl-warehouse:stop)
+      (setf *handler* nil))
+    (setf *handler* (cl-warehouse:start :port *port*))
+    (unwind-protect
+         (fiveam:run! 'all-tests)
+      (cl-warehouse:stop)
+      (setf *handler* nil))))
+
