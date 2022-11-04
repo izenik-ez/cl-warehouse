effb3d06fcef34b07b1d2baacea3309a28661dfd
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-11-04 12:51:21 +0100

    Add mito table schema and util functions. The server works

--- src/web.lisp
+++ src/web.lisp
@@ -20,6 +20,38 @@
 (defvar *web* (make-instance '<web>))
 (clear-routing-rules *web*)
 
+(defvar *alerts* '())
+
+(mito:connect-toplevel :sqlite3 :database-name
+                       (merge-pathnames #P"warehouse.db" *application-root*))
+(mito:deftable boxes ()
+  ((contents :col-type (:varchar 10))
+   (value :col-type (:integer))
+   (warehouse :col-type warehouses :references warehouses)))
+
+(mito:deftable boxes ()
+  ((contents :col-type (:varchar 50))
+   (value :col-type (:integer))
+   (warehouse :col-type warehouses :references warehouses)))
+
+;; utils
+(defun get-param (name parsed)
+  "Get param values from _parsed"
+  (cdr (assoc name parsed :test #'string=)))
+
+(defun get-order-by (direction sort-by)
+  "Construct sxql:order-by list values"
+  (let ((dir (if (string= direction "asc")
+                 :asc
+                 :desc))
+        (sortby (cond
+                  ((string= sort-by "location") :location)
+                  ((string= sort-by "capacity") :capacity)
+                  ((string= sort-by "contents") :contents)
+                  ((string= sort-by "value") :value)
+                  (t :id))))
+    (list dir sortby)))
+
 ;;
 ;; Routing rules
 
